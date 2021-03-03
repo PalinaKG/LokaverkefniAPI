ecaf38a06a27086129397aa6f0bf3f6136de7b47
Pálína Kröyer Guðmundsdóttir <palinakroyergudmundsdottir@Palinas-MacBook-Pro.local>
2021-03-03 14:48:13 +0000

    dfg

--- Django_app/MotionSickness/settings.py
+++ Django_app/MotionSickness/settings.py
@@ -156,7 +156,7 @@ STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
 
-#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 ALLOWED_HOSTS=['*']
