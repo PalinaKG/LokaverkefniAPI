f5e1e5f2ad83fb31eaa8482c292ab9aa0269b517
Pálína Kröyer Guðmundsdóttir <palinakroyergudmundsdottir@Palinas-MacBook-Pro.local>
2021-03-03 14:45:30 +0000

    9o

--- Django_app/MotionSickness/settings.py
+++ Django_app/MotionSickness/settings.py
@@ -67,6 +67,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'MotionSickness.urls'
@@ -155,7 +156,7 @@ STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 ALLOWED_HOSTS=['*']
