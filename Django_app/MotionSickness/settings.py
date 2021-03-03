d05528432208052017c5f0193bbb6cfe698d873d
Pálína Kröyer Guðmundsdóttir <palinakroyergudmundsdottir@Palinas-MacBook-Pro.local>
2021-03-03 14:58:16 +0000

    aerg

--- Django_app/MotionSickness/settings.py
+++ Django_app/MotionSickness/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'd&p^ttye03$wtp(i+p$b%#*uged2koe=#!-3yyah7k_b1)vdok'
+SECRET_KEY = 'd&p^ttye03$wtp(i+p$b%#*uged2koe=#!-3yyah7k_b1)vdok'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
