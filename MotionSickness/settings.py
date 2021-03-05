b93c6afdee63ba7fae16d411fe976b3baabef87c
Pálína Kröyer Guðmundsdóttir <palinakroyergudmundsdottir@Palinas-MacBook-Pro.local>
2021-03-05 10:24:49 +0000

    ger

--- MotionSickness/settings.py
+++ MotionSickness/settings.py
@@ -154,8 +154,8 @@ DATABASES = {
     'PASSWORD': 'password'
 }}
 
-# db_from_env = dj_database_url.config()
-# DATABASES['default'].update(db_from_env)
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
