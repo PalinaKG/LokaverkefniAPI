a32302f04387774e8fce0e448dc1b8378dc6fd86
Pálína Kröyer Guðmundsdóttir <palinakroyergudmundsdottir@Palinas-MacBook-Pro.local>
2021-03-08 11:51:36 +0000

    hello

--- HR/models.py
+++ HR/models.py
@@ -6,4 +6,5 @@ class hr(models.Model):
     hrid = models.AutoField(primary_key=True)
     subjectid = models.ForeignKey(subject, on_delete=models.CASCADE)
     bpm = models.DecimalField(max_digits=10, decimal_places=2) 
-    time = models.DecimalField(max_digits=10, decimal_places=2) 
+    time = models.DecimalField(max_digits=10, decimal_places=2)
+    #HELLOO 
